e1dc470e9634949fca16f753b3a579bd42c1c79b
Joseph Azevedo <joseph.az@gatech.edu>
2020-10-17 20:02:35 +0000

    feat: add basic CRA + typescript + emotion

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Global, css } from '@emotion/core'
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Global
+      styles={css`
+        body {
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+            'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+            sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+
+        code {
+          font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+            monospace;
+        }
+      `}
+    />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
