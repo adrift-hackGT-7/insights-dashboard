af7d9946182f04036b2ef1cf07abd20d0573e2ed
Joseph Azevedo <joseph.az@gatech.edu>
2020-10-18 11:40:28 +0000

    feat: update gist with cached real data from API

--- src/api.ts
+++ src/api.ts
@@ -38,9 +38,9 @@ export type HistoricalInsightApiItem = {
 export type HistoricalInsightApiItemState = "accepted" | "dismissed";
 
 const newInsightsUrl =
-  "https://gist.githubusercontent.com/jazevedo620/4389fa57afd403a4f56db6b92053154d/raw/3c5e44f511bf1716043d9f3b6c3ebd30fc0c993a/new-insights.json";
+  "https://gist.githubusercontent.com/jazevedo620/4389fa57afd403a4f56db6b92053154d/raw/f3e0e68711a3946d8f505355c380631fef5e571a/new-insights.json";
 const historicalInsightsUrl =
-  "https://gist.githubusercontent.com/jazevedo620/4389fa57afd403a4f56db6b92053154d/raw/3c5e44f511bf1716043d9f3b6c3ebd30fc0c993a/historical-insights.json";
+  "https://gist.githubusercontent.com/jazevedo620/4389fa57afd403a4f56db6b92053154d/raw/f3e0e68711a3946d8f505355c380631fef5e571a/historical-insights.json";
 
 export async function mockNewInsightsFetch(): Promise<NewInsightsApiResponse> {
   await new Promise((resolve) => setTimeout(resolve, 800));
