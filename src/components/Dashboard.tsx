671c3312442b423e0eb357ccdd878fd34710f0e2
Joseph Azevedo <joseph.az@gatech.edu>
2020-10-18 05:03:48 +0000

    fix: remove unneeded import

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "@emotion/styled";
 import { Redirect, Router, RouteComponentProps } from "@reach/router";
 import ReactSwitch from "react-switch";
